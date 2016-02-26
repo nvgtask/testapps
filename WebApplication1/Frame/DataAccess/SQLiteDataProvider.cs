1423d477ff8605f5eb64c0ee35c13c743bbfe475
stelan <stefan.landgraf@xmedia.ch>
2016-02-26 15:13:54 +0700

    initial

---
+++ WebApplication1/Frame/DataAccess/SQLiteDataProvider.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Frame.DataAccess
+{
+    public class SQLiteDataProvider : IDisposable
+    {
+        private SQLiteConnection dbConnection;
+
+        public SQLiteDataProvider()
+        {
+            dbConnection = new SQLiteConnection(@"Data Source=C:\is24exam\testapps\WebApplication1\App_Data\MyDatabase.sqlite;Version=3;");
+            dbConnection.Open();
+        }
+
+        public SQLiteCommand GetCommand(string sql)
+        {
+            return new SQLiteCommand(sql, dbConnection);
+        }
+
+        public void ExecuteNonQuery(string sql)
+        {
+            GetCommand(sql).ExecuteNonQuery();
+        }
+
+        public void Dispose()
+        {
+            dbConnection.Close();
+        }
+    }
+}
---
+++ WebApplication1/Global.asax.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using WebApplication1.Frame.DataAccess;
+
+namespace WebApplication1
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            using (var provider = new SQLiteDataProvider())
+            {
+                provider.ExecuteNonQuery("create table highscores (name varchar(100), score int)");
+                provider.ExecuteNonQuery("insert into highscores (name, score) values ('Jim', 10000)");
+                provider.ExecuteNonQuery("insert into highscores (name, score) values ('Mary', 3000)");
+                provider.ExecuteNonQuery("insert into highscores (name, score) values ('Paul', 9500)");
+                provider.ExecuteNonQuery("insert into highscores (name, score) values ('Kim', 2500)");
+                provider.ExecuteNonQuery("insert into highscores (name, score) values ('John', 8000)");
+            }
+        }
+    }
+}
